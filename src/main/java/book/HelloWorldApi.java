6d804593750d60e6af87804957ca1f5f14eaf7c9
karthik <karthik.androidz@gmail.com>
2022-06-03 09:41:00 +0100

    Weather API with DynamoDB

---
+++ src/main/java/book/HelloWorldApi.java
@@ -0,0 +1,11 @@
+package book;
+
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class HelloWorldApi {
+
+    public APIGatewayProxyResponseEvent handler(APIGatewayProxyRequestEvent event) {
+        return null;
+    }
+}
